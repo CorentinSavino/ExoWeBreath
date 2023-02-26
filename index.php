cb4788c39d0eeff8026ff038ab23387affe24a00
CorentinSavino <coresav03@outlook.fr>
2023-02-26 12:09:13 +0100

    Dashboard creation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>Bienvenue sur l'exercice des modules interactif par Corentin Savino</p>
+    <a href="./controller/Router.php?action=dashboard">Consulter le dashboard</a>
+</body>
+</html>
